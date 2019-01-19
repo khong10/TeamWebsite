71ee7bde0e4939f4ee5795ddda8818e9409a472e
Kahee Hong <khong10@myseneca.ca>
2019-01-19 12:10:59 -0500

    add routes for new images

--- server.js
+++ server.js
@@ -29,6 +29,27 @@ app.get("/project1", (req, res) => {
     res.sendFile(path.join(__dirname+"/views/project1.html"));
 });
 
+// Hackathon image file 1
+app.get("/image1", (req, res) => {
+    res.sendFile(path.join(__dirname+"/data/TD_Hackathon1.jpeg"));
+});
+
+// Hackathon image file 2
+app.get("/image2", (req, res) => {
+    res.sendFile(path.join(__dirname+"/data/TD_Hackathon2.jpeg"));
+});
+
+// Block-chain Hackathon image file 3
+app.get("/image3", (req, res) => {
+    res.sendFile(path.join(__dirname+"/data/blockchain_hackathon1.jpg"));
+});
+
+// Block- chain Hackathon image file 4
+app.get("/image4", (req, res) => {
+    res.sendFile(path.join(__dirname+"/data/blockchain_hackathon2.jpg"));
+});
+
+
 app.get('*', (req, res) => {
     res.status(404);
     res.redirect("https://cdn-images-1.medium.com/max/1600/1*2AwCgo19S83FGE9An68w9A.gif");
